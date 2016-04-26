b17952c44d4a9436e278684b14c9be788bc83ddf
Shaina Veloz <shainaveloz@Shainas-MacBook-Air.local>
2016-04-26 15:29:28 -0400

    added a refresh function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-$(document).ready(function(event){
+		$(document).ready(function(event){
 				var crystals = "";
 				var score;
 				var wins = 0;
-				var loses = 0;
-				var counter;
+				var losses = 0;
+				
 
 				var randomNumber = (function(min, max){
 					return Math.round(Math.random() * (max - min) + min);
@@ -19,45 +19,43 @@ $(document).ready(function(event){
 			    $('#button2').val(randomNumber(1,12));
 			    $('#button3').val(randomNumber(1,12));
 			    $('#button4').val(randomNumber(1,12));
+
+			    var refresh = function(event){
+			    	score = 0;
+			    	rand = randomNumber(19,120);
+			    	crystalVal = randomNumber(1,12);
+			    	$('#random').empty();
+			    	$('#random').append(rand);	
+					$('#button1').val(randomNumber(1,12));
+				    $('#button2').val(randomNumber(1,12));
+				    $('#button3').val(randomNumber(1,12));
+				    $('#button4').val(randomNumber(1,12));
+
+			    }
     
 			    var score = 0;
 			     $('button').click(function(){
 			        score = Number(score) + Number($(this).val());
-			        $('#score').text("Your total score is:"+score) + "<br>";
+			        $('#score').text("Your total score is: "+score) + "<br>";
 			        if (rand == score) {
-						wins++;
+						wins+=1;
 						alert("Congrats, YOU WIN !");
-						$("#wins").append("Wins = " + wins);
+						$("#wins").empty();
+						$("#wins").append("Wins: " + wins);
+						refresh();
+
 						console.log("wins = " + wins);
-						// location.reload();
 					}	
 					else if (rand < score) {
-						loses++;
+						losses+=1;
 						alert("YOU LOSE!!!");
-						$("#loses").append("Loses = " + loses);
-						console.log("loses = " + loses);
-						// location.reload();
+						$("#losses").empty();
+						$("#losses").append("Losses: " + losses);
+						console.log(losses);
+						refresh();
 					}
-
-					var html = "<p>Click any crystal to start playing</p>" + "<br><p>wins = " + wins + "<br><p>Loses = " + loses;
-
-			     $('#game').html(html);
 
 					
 			     });
 
-			     // var html = "<p>Click any crystal to start playing</p>" + "<br><p>wins = " + wins + "<br><p>Loses = " + loses;
-
-			     // $('#game').html(html);
-				
-					// console.log("wins = " + wins);
-					// console.log("loses = " + loses);
-					
-					// $("#wins").html = "Wins = " + wins;
-
-					// $("#loses").html = "Loses = " + loses;
-
-
 });
-
-			
